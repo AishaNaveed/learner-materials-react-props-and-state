ba76a471d1b6adb87df9e4e8bcb7d722e4ffd0ba
AishaNaveed <aisha@ailogica.com>
2021-11-21 18:38:50 +0000

    destructure props

--- src/components/CatCard.js
+++ src/components/CatCard.js
@@ -1,14 +1,14 @@
-const CatCard = (props) => {
+const CatCard = ({name, photo, alt, species, favFoods, birthYear}) => {
 
     //console.log("CatCard receiving props from App?! Come in App?! 😸 Props received are: ", props);
 
     return (
         <div className="card">
-            <h3 className="card__text card__header">{props.name}</h3>
-            <img className="card__image" src={props.photo} alt={props.alt}></img>
-            <p className="card__text">Species: {props.species}</p>
-            <p className="card__text">Favourite Food(s): {props.favFoods}</p>
-            <p className="card__text">Birth Year: {props.birthYear}</p>
+            <h3 className="card__text card__header">{name}</h3>
+            <img className="card__image" src={photo} alt={alt}></img>
+            <p className="card__text">Species: {species}</p>
+            <p className="card__text">Favourite Food(s): {favFoods}</p>
+            <p className="card__text">Birth Year: {birthYear}</p>
         </div>
     )
 }
